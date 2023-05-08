fea2ca831bc13ff4ce13be3c15b0c548412f2acd
Dylan Stockinger <pro.stockinger@gmail.com>
2023-05-08 19:05:29 +0200

    début du javascript, suite de la page "pokémons"

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+function toggleColor() {
+
+    if (document.getElementById("btn").style.backgroundColor = "#f07b3f") {
+        document.getElementById("btn").style.backgroundColor = "red";
+        document.getElementById("btn").innerHTML = "IM RED !"
+
+    } else {
+        document.getElementById("btn").style.backgroundColor = "#f07b3f"
+        document.getElementById("btn").innerHTML = "IM BACK BABY !"
+
+    }
+
+    }
+  
