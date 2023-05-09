4e81c8a28acd077ac998f8216366630f6a341b0e
Dylan Stockinger <pro.stockinger@gmail.com>
2023-05-09 22:28:02 +0200

    fetch ok, pokemon affiché sur la page "pokemons"

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
+const dataPokemonCards = document.querySelector(".main-container");
+
+fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
+  .then((res) => res.json())
+  .then((data) => {
+    dataPokemonCards.innerHTML = data.map((pokemon) => {
+      return `
+
+    <div class="pokemon-card" data-pokemon-cards>
 
 
-function toggleColor() {
+<img data-pokemon-image src="${pokemon.sprites.regular}" alt="">
+<div class="border-card">
+  <h3 data-name> ${pokemon.name.fr} </h3>
 
-    if (document.getElementById("btn").style.backgroundColor = "#f07b3f") {
-        document.getElementById("btn").style.backgroundColor = "red";
-        document.getElementById("btn").innerHTML = "IM RED !"
+  <div class="pokemon-type-details">
 
-    } else {
-        document.getElementById("btn").style.backgroundColor = "#f07b3f"
-        document.getElementById("btn").innerHTML = "IM BACK BABY !"
+  <div class="button-call">
+    <button  class="call-to-action" id="btn"> CLICK </button>
 
-    }
+  </div>
+</div>
+</div>
+</div>
+ `;
+    });
+  });
 
-    }
-  
-    
+const filterPokemonGeneration = document.querySelector(".generation-container");
+console.log(filterPokemonGeneration);
+
+fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
+  .then((res) => res.json())
+  .then((data) => {});
