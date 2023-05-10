ab40bb30ad6f9851f5eec391cbd00732ae97e66e
Dylan Stockinger <pro.stockinger@gmail.com>
2023-05-10 11:48:28 +0200

    Refactorisation du code au niveau du fetch pour une plus grande lisibilité next : searchbar

--- js/index.js
+++ js/index.js
@@ -1,34 +1,43 @@
 const dataPokemonCards = document.querySelector(".main-container");
+const searchBar = document.getElementById("searchbar");
+let pokemonCharacters = [];
 
-fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
-  .then((res) => res.json())
-  .then((data) => {
-    dataPokemonCards.innerHTML = data.map((pokemon) => {
-      return `
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+});
 
-    <div class="pokemon-card" data-pokemon-cards>
-
-
-<img data-pokemon-image src="${pokemon.sprites.regular}" alt="">
-<div class="border-card">
-  <h3 data-name> ${pokemon.name.fr} </h3>
-
-  <div class="pokemon-type-details">
-
-  <div class="button-call">
-    <button  class="call-to-action" id="btn"> CLICK </button>
-
-  </div>
-</div>
-</div>
-</div>
- `;
-    });
+const loadCharacters = async () => {
+  try {
+    const res = await fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon");
+    pokemonCharacters = await res.json();
+    displayCharacters(pokemonCharacters);
+  } catch (err) {
+    console.error(err);
+  }
+};
+const displayCharacters = (characters) => {
+  const htmlString = characters.map((pokemon) => {
+    return `
+        <div class="pokemon-card" data-pokemon-cards>
+    
+    
+    <img data-pokemon-image src="${pokemon.sprites.regular}" alt="">
+    <div class="border-card">
+      <h3 data-name> ${pokemon.name.fr} </h3>
+    
+      <div class="pokemon-type-details">
+    
+      <div class="button-call">
+        <button  class="call-to-action" id="btn"> CLICK </button>
+    
+      </div>
+    </div>
+    </div>
+    </div>
+     `;
   });
 
-const filterPokemonGeneration = document.querySelector(".generation-container");
-console.log(filterPokemonGeneration);
+  dataPokemonCards.innerHTML = htmlString;
+};
 
-fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
-  .then((res) => res.json())
-  .then((data) => {});
+loadCharacters();
