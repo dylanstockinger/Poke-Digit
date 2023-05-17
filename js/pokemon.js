5f7b1170f45058477b3c8069ccc4934ce8c17c2e
Dylan Stockinger <pro.stockinger@gmail.com>
2023-05-17 15:35:45 +0200

    changement du background color en fonction du type du pokémon (plante : vert etc..)

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,10 +4,13 @@ const pokeContainer = document.querySelector(".pokemon-container");
 
 const displayCharacter = (pokemon) => {
   pokeContainer.innerHTML = `
-  <img data-pokemon-image src="${pokemon.sprites.regular}" alt="">
-  <h1> ${pokemon.name.fr}</h1>
-  
-  
+  <div class="pokemon-container">
+    <div class="background-container">
+      <img data-pokemon-image src="${pokemon.sprites.regular}" alt="">
+      <h1> ${pokemon.name.fr}</h1>
+      ${pokemon.types.map((type) => `<h1>${type.name}</h1>`).join("")}
+    </div>
+  </div>
   `;
 };
 
@@ -18,11 +21,49 @@ getOnePokemon = async () => {
     );
     pokemonCharacter = await res.json();
     displayCharacter(pokemonCharacter);
+    backgroundColorChangeToggle(pokemonCharacter.types[0].name);
 
     console.log(pokemonCharacter);
   } catch (err) {
     console.error(err);
   }
 };
+backgroundColorChangeToggle = (type) => {
+  if (type === "Plante") {
+    document.body.style.backgroundColor = "green";
+  }
+  switch (type) {
+    case "Plante":
+      document.body.style.backgroundColor = "green";
+      break;
+    case "Poisson":
+      document.body.style.backgroundColor = "blue";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case "Feu":
+      document.body.style.backgroundColor = "red";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case "Eau":
+      document.body.style.backgroundColor = "#44A9C8";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case "Insecte":
+      document.body.style.backgroundColor = "#A7BC5D";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case "Normal":
+      document.body.style.backgroundColor = "#E1DAAA";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case "Poison":
+      document.body.style.backgroundColor = "#7D6A96";
+      // Expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
+};
 
 getOnePokemon();
+backgroundColorChangeToggle();
